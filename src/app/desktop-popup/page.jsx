96b0d591257ee80daf5380271c29888ee33c1e21
Nancy <rishi3dec@gmail.com>
2024-10-18 19:58:30 +0530

    popup

--- src/app/desktop-popup/page.jsx
+++ src/app/desktop-popup/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import desktopHero from "../../../public/desktopHero.png";
+import desktopHero from "../../../public/h.png";
 import DesktopForm from "../components/DesktopForm";
 import WAIMG from "../../../public/whatsapp.png";
 
